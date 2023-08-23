518b4b2ea9d8eb9ec8127c515a3d197388ddc88b
Mahmoud Abdullah Anani <99493135+MahmoudAbdullahAnani@users.noreply.github.com>
2023-08-23 12:20:26 +0300

    soliution error

--- services/orderService.js
+++ services/orderService.js
@@ -186,7 +186,7 @@ const checkoutCompletedService = async (req, res) => {
   const endpointSecret = process.env.endpoint_checkout_completed_secret;
   const sig = req.headers["stripe-signature"];
 
-  console.log("req.data.object", req.data.type);
+  console.log("req.data", req.data);
 
   let eventCheckOut;
 
